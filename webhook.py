ab9315b19dafb999ba46763e40bd1b1ff7aa731a
Lorem1377 <129212991+tibla@users.noreply.github.com>
2025-07-09 17:39:19 +0300

    webhook.py

--- webhook.py
+++ webhook.py
@@ -1,4 +1,4 @@
 from discord_webhook import DiscordWebhook
 
-webhook = DiscordWebhook(url="https://discord.com/api/webhooks/1392514465240580195/KYzEZHcGEwSmJyLlxuP9STfaKh2yVUmM4OCzBMamn2-RQZ2rYQgIO69IbDUopA7nb1UG", content="Тестов1ое Сообщение.")
+webhook = DiscordWebhook(url="https://discord.com/api/webhooks/1392514465240580195/KYzEZHcGEwSmJyLlxuP9STfaKh2yVUmM4OCzBMamn2-RQZ2rYQgIO69IbDUopA7nb1UG", content="хуй")
 response = webhook.execute()
