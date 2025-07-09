209816fa7b30b4cd30281b9cc49742a5984750fe
Lorem1377 <129212991+tibla@users.noreply.github.com>
2025-07-09 17:52:23 +0300

    webhook.py

--- webhook.py
+++ webhook.py
@@ -1,4 +1,13 @@
 from discord_webhook import DiscordWebhook
 
-webhook = DiscordWebhook(url="https://discord.com/api/webhooks/1392514465240580195/KYzEZHcGEwSmJyLlxuP9STfaKh2yVUmM4OCzBMamn2-RQZ2rYQgIO69IbDUopA7nb1UG", content="х111111111уй")
+webhook = DiscordWebhook(url="https://discord.com/api/webhooks/1392514465240580195/KYzEZHcGEwSmJyLlxuP9STfaKh2yVUmM4OCzBMamn2-RQZ2rYQgIO69IbDUopA7nb1UG", username="Калл")
+
+# send two images
+with open("path/to/first/image.jpg", "rb") as f:
+    webhook.add_file(file=f.read(), filename="example.jpg")
+with open("path/to/second/image.jpg", "rb") as f:
+    webhook.add_file(file=f.read(), filename="example2.jpg")
+# remove "example.jpg"
+webhook.remove_file("example.jpg")
+# only "example2.jpg" is sent to the webhook
 response = webhook.execute()
