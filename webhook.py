3d2dc63dc5218c8240d1a6a4ddcea0064547929d
Lorem1377 <129212991+tibla@users.noreply.github.com>
2025-07-09 17:35:40 +0300

    webhook.py

--- webhook.py
+++ webhook.py
@@ -1,4 +1,4 @@
 from discord_webhook import DiscordWebhook
 
-webhook = DiscordWebhook(url="https://discord.com/api/webhooks/1392435872833601627/z0ErCKfbI4ncH8HfOq-QJi6yYeLqcq8cj6RQlKgrP1rZoJmIuQahLwsVUUOCOwa-aDzn", content="Тестовое Сообщение.")
+webhook = DiscordWebhook(url="https://discord.com/api/webhooks/1392514465240580195/KYzEZHcGEwSmJyLlxuP9STfaKh2yVUmM4OCzBMamn2-RQZ2rYQgIO69IbDUopA7nb1UG", content="Тестовое Сообщение.")
 response = webhook.execute()
